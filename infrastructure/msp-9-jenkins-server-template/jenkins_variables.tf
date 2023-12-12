9c9d24d9b376dc5775e62fe37a5c556a3a3a687b
demirozman <demirozman571@gmail.com>
2023-12-12 20:02:37 +0000

    moving from dev to jenkins server for CI/CD pipeline configuration

---
+++ infrastructure/msp-9-jenkins-server-template/jenkins_variables.tf
@@ -0,0 +1,10 @@
+variable "mykey-a" {}
+variable "ami-a" {
+  description = "amazon linux 2023 ami"
+}
+variable "region" {}
+variable "instance_type_a" {}
+variable "jenkins_server_secgr_a" {}
+variable "jenkins-server-tag-a" {}
+variable "jenkins-profile-a" {}
+variable "jenkins-role-a" {}
